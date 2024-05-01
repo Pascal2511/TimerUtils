5c68b87be4c48cd6cf9fe1e621fd1fe911c7df38
Pascal2511 <115885966+Pascal2511@users.noreply.github.com>
2024-05-01 18:53:36 +0200

    Update TimerUtils.cpp

--- scr/TimerUtils.cpp
+++ scr/TimerUtils.cpp
@@ -1,13 +1,23 @@
 #include "TimerUtils.h"
 
-TimerUtils::TimerUtils() {
-  lastTime = 0;
+TimerUtils::TimerUtils(int numIntervals) {
+  NUM_INTERVALS = numIntervals; // Initialize NUM_INTERVALS based on the parameter
+  lastTimes = new unsigned long[NUM_INTERVALS]; // Dynamically allocate memory for lastTimes array
+  for (int i = 0; i < NUM_INTERVALS; ++i) {
+    lastTimes[i] = 0;
+  }
 }
 
-bool TimerUtils::everyMs(unsigned long interval) {
-  if (millis() - lastTime > interval) {
-    lastTime = millis();
+TimerUtils::~TimerUtils() {
+  delete[] lastTimes; // Deallocate memory for lastTimes array
+}
+
+bool TimerUtils::everyMs(unsigned long interval, int index) {
+  unsigned long currentTime = millis();
+  if (currentTime - lastTimes[index] >= interval) {
+    lastTimes[index] = currentTime; // Update last time for the specified interval
     return true;
   }
   return false;
 }
+
